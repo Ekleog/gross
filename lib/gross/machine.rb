8104814459cbaa9488d73ec0d862652c2982445b
Ekleog <LeoG@spard.me>
2015-08-17 08:42:27 +0200

    Remove unneeded require

--- lib/gross/machine.rb
+++ lib/gross/machine.rb
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with gross.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'thread'
 require 'gross/task'
 
 module Gross
